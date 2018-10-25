75ae2e6f525e99edd47a364c4782a5dbc24e9ae3
Hasan Tayyar BEŞİK <tayyar.besik@gmail.com>
2018-10-25 21:10:47 +0200

    prettier

--- src/builder.ts
+++ src/builder.ts
@@ -3,29 +3,34 @@ import * as fs from 'fs-extra';
 import * as walker from 'klaw-sync';
 import { render } from './template';
 
-const createPage = async function(item: walker.Item) {
-  console.log('Create index for', item.path);
-  const pageContent = await createContent(item.path);
-  fs.writeFileSync(`${item.path}/index.html`, pageContent);
+const createPage = async function(path: string) {
+  console.log('Create index for', path);
+  const pageContent = await createContent(path);
+  fs.writeFileSync(`${path}/index.html`, pageContent);
 };
 
-const createContent = async (path: string): Promise<string> => {
+const createContent = async (filePath: string): Promise<string> => {
+  const rootPath = path.resolve('./pages');
   return render(
-    (await list(path, txtFilter)).map(x => x.path.split('/').pop())
+    (await list(filePath, contentFilter)).map((x: walker.Item) =>
+      x.path.replace(rootPath + '/', '')
+    )
   );
 };
-const txtFilter: walker.Filter = (item: walker.Item) =>
-  path.extname(item.path) === '.txt';
+const contentFilter: walker.Filter = (item: walker.Item) =>
+  path.extname(item.path) === '.txt' || item.stats.isDirectory();
+
 const dirFilter: walker.Filter = (item: walker.Item) =>
   item.stats.isDirectory();
 
 const list = async (path: string, filter?: walker.Filter) =>
-  walker(path, { filter, depthLimit: 1 });
+  walker(path, { filter, depthLimit: 0 });
 
 export const build = async (path: string) => {
+  await createPage(path);
   const dirs = await list(path, dirFilter);
   await dirs.forEach(async dir => {
-    await createPage(dir);
+    await createPage(dir.path);
     await list(dir.path);
   });
   return dirs;
